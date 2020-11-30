199550de2062807d691bd14832eac3ef2383c72d
ahujab <dhwanildesai@Dhwanils-MacBook-Air.local>
2020-11-30 18:14:32 -0500

    final

--- backend/index.js
+++ backend/index.js
@@ -2,127 +2,97 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require('axios');
 const ws = require('ws');
-// Initialize express and define a port
+const cors = require('cors')
 
-const wss = new ws.Server({port: 4890})
+
+const wss = new ws.Server({ port: 4890 })
 const app = express()
 const PORT = 3000
 const path = require('path');
 const http = require('http');
 const { response } = require("express");
-const queryString=require("querystring");
+const queryString = require("querystring");
 const fs = require('fs');
 const readJson = fs.readFileSync(path.resolve(__dirname, './views/data/series.json'));
 const data = JSON.parse(readJson);
 
-wss.on('connection',(ws)=>{
-  ws.on('message',(message)=>{
-
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
     console.log("ws message")
   })
-  // ws.send("hello");
-
+  
 })
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.renderWithData = function (view, model, data) {
-      res.render(view, model, function (err, viewString) {
-          data.view = viewString;
-          res.json(data);
-      }); 
+    res.render(view, model, function (err, viewString) {
+      data.view = viewString;
+      res.json(data);
+    });
   };
   next();
 });
-// app.use('/', function(req,res){
-//   res.sendFile(path.join(__dirname+'/views/index.ejs'));
-//   //__dirname : It will resolve to your project folder.
-// });
+app.use(cors())
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 app.post("/baks", (req, res) => {
-    console.log(req.body);
-  
-    // return a text response
-    const {body}=req;
-  wss.clients.forEach((client)=>{
+  console.log(req.body);
+
+  const { body } = req;
+  wss.clients.forEach((client) => {
     client.send(JSON.stringify(body));
   })
-    //res.send(req.body.id,req.body.status); 
-     //res.redirect('/baks2?',queryString.stringify(body.data))
-     const { id, status } = req.body;
-    data.id = id;
-    data.status = status;
-    //  data.push({ id: id, status: status});
-    //  fs.writeFileSync(path.resolve(__dirname, './views/data/series.json'), JSON.stringify(data, null, 4));
-     res.redirect('/calls');
-
-    //   res.render('/baks2', {
-    //   callid: body.id,
-    //  callstatus:body.status
-    //  });
-    })
-  
-  
-
-    app.get("/baks", (req, res) => {
-      console.log(req.body);
-      
-      // return a text response
-      const {body}=req;
-      res.send(req.body.id,req.body.status); 
-       return res.render('calldetail', {
-        callid: body.id,
-       callstatus:body.status
-       });
-      });
-  
-app.get("/baks2", (req, res) => {
-    
-var data = JSON.stringify({"phone":"13018040009","webhookURL":"http://localhost:3000/baks"});
-
-var config = {
-  method: 'options',
-  url: 'http://localhost:4830/call',
-  headers: { 
-    'Content-Type': 'application/json'
-  },
-  data : data
-};
-axios(config)
-.then(function (response) {
-  
  
-  console.log(JSON.stringify(response.data));
-  return res.render('index', {
-    callid: response.data.id,
-    callstatus:"gg"
-   
+  const { id, status } = req.body;
+  data.id = id;
+  data.status = status;
+  
+  res.redirect('/calls');
+})
 
+
+
+app.get("/baks", (req, res) => {
+  
+  // return a text response
+  const { body } = req;
+  res.send(req.body.id, req.body.status);
+  return res.render('calldetail', {
+    callid: body.id,
+    callstatus: body.status,
     
   });
-  res.send("Thank you")
-})
-.catch(function (error) {
-  console.log(error);
 });
 
+app.get("/baks2", (req, res) => {
+  let param = req.query.phone
+  var data = JSON.stringify({ "phone": param.toString(), "webhookURL": "http://localhost:3000/baks" });
 
-
-
-
-    //res.json(data);
+  var config = {
+    method: 'options',
+    url: 'http://localhost:4830/call',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+  axios(config)
+    .then(function (response) {
+      console.log("hi"+JSON.stringify(response.data));
+      res.send(response.data)      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 app.get("/calls", (req, res) => {
   console.log("in call ")
-  res.render('index',{callid:data.id, callstatus:data.status})
+  res.render('index', { callid: data.id, callstatus: data.status })
 });
 
 const server = http.createServer(app);
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-server.listen(PORT ,() => console.log(`🚀 Server running on port ${PORT}`));
-
-
-// Start express on the defined port
-//app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
--- src/Home.js
+++ src/Home.js
@@ -1,120 +1,111 @@
 import React, { Component } from 'react'
-//import './Home.css'
-import Call from './Call.js'
+import './Home.css'
+
 import text from './inputnumberstocall.js'
-//import TableRow from './TableRow';
+import axios from 'axios';
+
 import { w3cwebsocket } from "websocket";
 
 const client = new w3cwebsocket('ws://127.0.0.1:4890');
-const newarr = text.split(" ");
-
+const newarrofnumbers = text.split(" ");
+var idreturn = ""
+var dict = {}
 class Home extends Component {
-   
+
     constructor(props) {
         super(props)
-        this.state =  {
-            button:false,
-            start:false,
-            data:[{phone_identifier:"1234_1",phone_number:"1234",phone_status:"idle",phone_id:"12"}
-        ,{phone_identifier:"12342_1",phone_number:"1234",phone_status:"idle",phone_id:"12"}
-          ]
-    }}
-    callprocess(e){
-        console.log(e.currentTarget.value)
-        this.setState({
-        data: this.state.data.map((element)=>{
-            if(element.phone_identifier==e.currentTarget.value){
-                return {...element, phone_status:"calling"}
-            }
-            return element
+        var allnumbersstate = []
+        newarrofnumbers.map((e) => {
+            allnumbersstate.push({ phone_identifier: e + "_1", phone_number: e, phone_status: "idle", phone_id: "12" })
+        })
 
-        }
-        )})
-    
+        newarrofnumbers.map((e) => {
+            this.state = {
+                data: allnumbersstate
+            }
+        })
     }
+
+    async callprocess(e) {
+        console.log("on click id" + e.currentTarget.value)
+        const id = e.currentTarget.value
+        const phone = id.substring(0, 11)
+        console.log(phone)
+
+
+        await axios.get('http://localhost:3000/baks2', { params: { "phone": phone } })
+            .then((res) => {
+                idreturn = res.data.id
+                console.log(res)
+                return res
+            }).catch((error) => {
+                console.log(error)
+            });
+        console.log("came " + idreturn)
+
+        dict[idreturn] = id;
+        console.log("dict" + dict[idreturn])
+
+    }
+
     componentWillMount() {
 
         client.onopen = () => {
             console.log("open");
-
         }
         client.onmessage = (data) => {
             console.log(data);
+            const parsed = JSON.parse(data.data)
+           
             this.setState({
-                data: JSON.parse(data.data)
+                data: this.state.data.map((element) => {
+                    if (element.phone_identifier == dict[parsed.id]) {
+                        console.log("match")
+                        return { ...element, phone_status: parsed.status, phone_id: parsed.id }
+                    }
+                    return element
+                }
+                )
             })
+
         }
     }
+
     render() {
-        const {data} = this.state;
-        console.log(data)
-        // const rows = 
-            //  <Call id={data.id} status={data.status}/>
-        
+        const { data } = this.state;
+
+
         return (
             <div>
-          <form onSubmit>
-          <input
-          type="submit" disabled={this.state.button} value={"start"}
-          
-        />
-          </form>
+
+                <div><button onClick={(e) => this.callprocess(e)}
+                    disabled={false} value={data[0].phone_identifier}
+
+                ><span>{<p>"Call Now"</p>}</span></button>
+                </div>
                 <table >
-                <thead>
-                <tr>
-                <th>Phone</th>
-                <th>status</th>
-              </tr>
-                </thead>
+                    <tbody>
 
-                <tbody>
-                {data.map((item, i) => {
-                    console.log(item)
-                    // return <div>{"hi"}</div>
-                    return <div> <h1> {item.phone_number}</h1>
-                    <h1> {item.phone_id}</h1>
-                    <h1> {item.phone_status}</h1>
-                    <div><button onClick={(e)=>this.callprocess(e)}
-                        disabled={false} value={item.phone_identifier} >
-                    <span>{<p>"Call Now"</p>} </span>
-                    </button></div></div>
+                        <div>
+                            {data.map((item, i) => {
+                                console.log(item)
+                                // return <div>{"hi"}</div>
+                                return <div className="main">Phone Number: <strong>{item.phone_number}</strong>
+                                    <th><td>Phone id: {item.phone_id}</td></th>
+                                    <th>Phone status: {item.phone_status}</th>
+                                </div>
+                            })}
 
-                })}
-                  {/* {rows} */}
-                  
-                  </tbody>
-                  
+
+                        </div>
+                    </tbody>
+
                 </table>
-          </div>
+            </div>
 
-            
-                
-            //     {console.log(this.state.message)}
-            //     {this.state.message.status}
-            //     </div>
         )
     }
 }
 
-// function Home() {
-//     const newarr = text.split(" ");
-
-//     return (
-//         <div className="home">
-
-
-//                     {newarr.map((item, i) => {
-
-//                         return <Call id={item}/>
-
-//                     })}
-
-
-
-
-
-//         </div>
-//     );
-// }
 
 export default Home;
